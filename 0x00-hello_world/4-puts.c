cec0c5ce2594f2d152d6ca76c6b0d47e4e665cf6
Alistair-Sabe <alistairsabe96@gmail.com>
2022-12-08 02:02:55 -0800

    print using _puts_ function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
-#include <studio.h>
+#include <stdio.h>
 int main(){
-	puts("Programming is like building a multilingual puzzle");
+	puts("Programming is like building a multilingual puzzle\n");
 
 	getchar();
 	return 0;
+}
