b23b492f14540495dd793bfaf62f60b38fa9cb67
Alistair-Sabe <alistairsabe96@gmail.com>
2022-12-09 15:11:33 -0800

    task1: print the last digit of the number stored in the variable

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,34 @@
+#include <stdio>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	printf("%d ", n)
+	/* your code goes there */
+	printf("Last digit of %d is %d ", n)
+	if(n > 5)
+	{
+	printf("and is greater than 5\n")
+	}
+	else if!(n >= 6)
+	{
+	printf("and is less than and not 0\n")
+	}
+	else
+	{
+	printf("and is 0\n")
+	}
+	return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,2 +1,6 @@
 task0: program will assign a random number to the variable n each time it is executed. Complete the source code in order to print whether the number stored in the variable n is positive or negative
+code: printf("%d ", _variable_) -> generates random number
+
+task1: print the last digit of the number stored in the variable
 code:
+
