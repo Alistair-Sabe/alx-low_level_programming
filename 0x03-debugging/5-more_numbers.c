de5a3200786361af55c0f00befb516efc0fd27bf
Alistair-Sabe <alistairsabe96@gmail.com>
2022-12-15 20:46:24 -0800

    task5: function that prints 10 times the numbers, from 0 to 14

---
+++ 0x03-debugging/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * more_numbers - prints 10 times the numbers, from 0 to 14
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int m, n;
+
+	for (m = 1; m <= 10; m++)
+	{
+		for (n = 0; n <= 14; n++)
+		{
+			if (n >= 10)
+				putchar('1');
+			putchar (n % 10 + '0');
+		}
+		putchar('\n');
+	}
+}
