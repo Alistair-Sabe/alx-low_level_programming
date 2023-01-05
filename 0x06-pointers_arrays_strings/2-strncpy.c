c4f4101ec616246f8d7e1fb25c5ecb9f047a96ea
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 <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>
2023-01-05 13:00:06 -0800

    task3: function that copies a string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,5 +1,7 @@
 #include "main.h"
 
+
+
 /**
  * _strncpy - Copies at most an inputted num of bytes from src string
  * @dest: The buffer storing the string copy
@@ -10,6 +12,7 @@
  */
 
 char *_strncpy(char *dest, char *src, int n)
+
 {
 
 	int index = 0, src_len = 0;
@@ -18,7 +21,7 @@ char *_strncpy(char *dest, char *src, int n)
 		src_len++;
 
 	for (index = 0; src[index] && index < n; index++)
-		dest[index] = '\0';
+		dest[index] = src[index];
 	for (index = src_len; index < n; index++)
 		dest[index] = '\0';
 
