055ca4613ec1e4c12cba51a1997c7340ad927bab
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 <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>
2023-01-05 12:54:58 -0800

    task3: function that copies a string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -6,16 +6,20 @@
  * @src: Source string
  * @n: Maximum number of bytes to copied from src
  * Return: A pointer to the result dest
+ *
  */
 
 char *_strncpy(char *dest, char *src, int n)
 {
+
 	int index = 0, src_len = 0;
 
 	while (src[index++])
 		src_len++;
 
 	for (index = 0; src[index] && index < n; index++)
+		dest[index] = '\0';
+	for (index = src_len; index < n; index++)
 		dest[index] = '\0';
 
 	return (dest);
