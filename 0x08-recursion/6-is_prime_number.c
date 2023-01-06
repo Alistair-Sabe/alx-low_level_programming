b2b6b56c6a54482a2804512e67f2950cbc38828c
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 <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>
2023-01-06 02:39:02 -0800

    task6: function that returns 1 if the input integer is a prime number, otherwise return 0

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+
+int is_divisible(int num, int div);
+
+/**
+ * is_prime_number -  Return if num is prime
+ * @n: num to check
+ * Return: int value
+ */
+int is_prime_number(int num)
+{
+	return (is_divisible(num, 1));
+}
+/**
+ * is_divisible - Check if num is divisible
+ * @num: Num to check i divisble
+ * @div: divisor
+ * Return: If num is divisible -0
+ * If num is not divisible -1
+ */
+
+int is_divisible(int num, int div)
+{
+	if (num <= 1)
+		return (0);
+
+	if (num % div == 0 && div > 1)
+		return (0);
+
+	if ((num / div) < div)
+		return (1);
+
+	return (is_divisible(num, div + 1));
+}
