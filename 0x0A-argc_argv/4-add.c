fabc6c929e38a40c5733f4e42695fcddc9c2d09c
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 <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>
2023-01-09 09:39:22 -0800

    task4: program that adds positive numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - adds positive numbers from args
+ * @agc: arg count
+ * @argv: arg vector
+ * Return: zero
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+	unsigned int x, sum = 0;
+	char *a;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			a = argv[i];
+
+			for (x = 0; x < strlen(a); x++)
+			{
+				if (a[x] < 48 || a[x] > 57)
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+			
+			sum += atoi(a);
+			a++;
+		}
+
+		printf("%d\n", sum);
+	}
+	else
+	{
+		printf("0\n");
+	}
+
+	return (0);
+}
