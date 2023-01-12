f0f811f97ca17e8e1355058644742ece4868c358
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 <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>
2023-01-12 14:02:00 -0800

    task1: function that returns a pointer to a newly allocated space in memory, which contains a copy of the string given as a parameter

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * *_strdup - copies string given as parameter
+ * @str: string to duplicate
+ * Return: pointer to the copied string (NULL)
+ */
+
+char *strdup(char *str)
+{
+	char *duplicate;
+	unsigned int i, ln;
+
+	i = 0;
+	ln = 0;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[ln])
+		ln++;
+	duplicate = malloc(sizeof(char) * (ln + 1));
+
+	if (duplicate == NULL)
+		return (NULL);
+
+	while ((duplicate[i] = str[i]) != '\0')
+		i++;
+
+	return (duplicate);
+}
