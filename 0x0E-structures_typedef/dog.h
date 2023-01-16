e80ca9aeb5bdb1efac821ab4a9d937da50d83e66
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 <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>
2023-01-16 07:18:04 -0800

    Add putchar.c

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,10 +15,10 @@ struct dog
 	float age;
 	char *owner;
 };
- typedef struct dog dog_t;
- void init_dog(struct dog *d, char *name, folat age, char *owner);
- void init_dog(struct dog *d);
- dog_t *new_dog(char *name, float age, char *owner);
- void free_dog(dog_t *d);
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, folat age, char *owner);
+void init_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
