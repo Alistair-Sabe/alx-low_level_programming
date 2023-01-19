28eb172cd6d641bc725739b992b83c2060616ba6
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 <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>
2023-01-19 12:49:19 -0800

    task0: function that prints a name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,14 +1,13 @@
 #include "function_pointers.h"
-#include <stdio.h>
+
 /**
  * print_name - print name using pointer
  * @name: name to be printed
  * @f: poineter to function
- * Return: void
+ * Return: Nothing
  */
-
 void print_name(char *name, void (*f)(char *))
 {
-	if (name != NULL || f != NULL)
+	if (name != NULL && f != NULL)
 		f(name);
 }
